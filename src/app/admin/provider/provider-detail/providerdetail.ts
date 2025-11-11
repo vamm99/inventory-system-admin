71a3a4a0472931236a9e84190c1a766cce138ccc
victor <130195315+vamm99@users.noreply.github.com>
2025-11-11 18:03:14 -0500

    feat: servicio de provider

---
+++ src/app/admin/provider/provider-detail/providerdetail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-providerdetail',
+  imports: [],
+  templateUrl: './providerdetail.html',
+  styleUrl: './providerdetail.css',
+})
+export class Providerdetail {
+
+}
---
+++ src/app/admin/provider/service/provider.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from "@angular/core";
+import { API_URL } from "../../../api/api";
+import { HttpClient } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Observable } from "rxjs";
+
+interface Pagination {
+    limit: number;
+    page: number;
+    total: number;
+    lastPage: number;
+}
+
+interface Provider {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+interface ProviderShare {
+    name?: string;
+    email?: string;
+    phone?: string;
+}
+
+export type ProviderResponse<T> = {
+    status: number;
+    message: string;
+    data: T;
+    pagination?: Pagination;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProviderService {
+    private apiUrl = API_URL;
+    private http = inject(HttpClient);
+
+    createProvider(provider: Provider): Observable<ProviderResponse<Provider>> {
+        const response = this.http.post<ProviderResponse<Provider>>(`${this.apiUrl}/provider`, provider);
+        return response;
+    }
+
+    getAllProviders(page: number = 1, limit: number = 10): Observable<ProviderResponse<Provider[]>> {
+        const response = this.http.get<ProviderResponse<Provider[]>>(`${this.apiUrl}/provider?page=${page}&limit=${limit}`);
+        return response;
+    }
+
+    getProviderById(id: number): Observable<ProviderResponse<Provider>> {
+        const response = this.http.get<ProviderResponse<Provider>>(`${this.apiUrl}/provider/${id}`);
+        return response;
+    }
+
+    updateProvider(id: number, provider: Provider): Observable<ProviderResponse<Provider>> {
+        const response = this.http.put<ProviderResponse<Provider>>(`${this.apiUrl}/provider/${id}`, provider);
+        return response;
+    }
+
+    deleteProvider(id: number): Observable<ProviderResponse<Provider>> {
+        const response = this.http.delete<ProviderResponse<Provider>>(`${this.apiUrl}/provider/${id}`);
+        return response;
+    }
+
+    shareProviderByName(providerShare: ProviderShare): Observable<ProviderResponse<Provider[]>> {
+        const { name, email, phone } = providerShare;
+        let url = `${this.apiUrl}/provider/share`;
+        if (name) url += `?name=${name}`;
+        if (email) url += `?email=${email}`;
+        if (phone) url += `?phone=${phone}`;
+        const response = this.http.get<ProviderResponse<Provider[]>>(url);
+        return response;
+    }
+}
